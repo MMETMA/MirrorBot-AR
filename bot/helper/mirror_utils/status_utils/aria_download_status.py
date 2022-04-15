1e6109389993550a5d1ea4d49bd255b9cba90398
MMETMA <79155572+MMETMA@users.noreply.github.com>
2022-04-15 22:36:25 +0200

    Add files via upload

--- bot/helper/mirror_utils/status_utils/aria_download_status.py
+++ bot/helper/mirror_utils/status_utils/aria_download_status.py
@@ -99,14 +99,14 @@ class AriaDownloadStatus(Status):
         LOGGER.info(f"Cancelling Download: {self.name()}")
         download = self.aria_download()
         if download.is_waiting:
-            self.__listener.onDownloadError("Cancelled by user")
+            self.__listener.onDownloadError("تم الايقاف بواسطة المستخدم")
             aria2.remove([download], force=True)
             return
         if len(download.followed_by_ids) != 0:
             downloads = aria2.get_downloads(download.followed_by_ids)
-            self.__listener.onDownloadError('Download stopped by user!')
+            self.__listener.onDownloadError('تم الايقاف بواسطة المستخدم!')
             aria2.remove(downloads, force=True)
             aria2.remove([download], force=True)
             return
-        self.__listener.onDownloadError('Download stopped by user!')
+        self.__listener.onDownloadError('تم الايقاف بواسطة المستخدم')
         aria2.remove([download], force=True)   
